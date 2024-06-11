db651712263f020f4619496803b6ffb4ed9aeeca
Moodle Workplace <workplace@moodle.com>
2024-06-11 19:17:37 +0000

    Moodle Workplace 4.4.1 Rolling (Build: 20240611)

--- tool_organisation.php
+++ tool_organisation.php
@@ -293,6 +293,7 @@ $string['movedepartmentframework'] = 'Move department framework \'{$a}\'';
 $string['movepositionframework'] = 'Move position framework \'{$a}\'';
 $string['newframework'] = 'New framework';
 $string['newnamefor'] = 'New name for \'{$a}\'';
+$string['newuseradded'] = 'A new user has been added. <a href="{$a}">View jobs and reporting lines for this user</a>';
 $string['notactivejob'] = 'Not active';
 $string['notification'] = 'Notification';
 $string['notificationcannotcreatejobs'] = 'Before assigning jobs to users, create some departments and positions.';
